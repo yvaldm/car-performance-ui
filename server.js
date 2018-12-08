a94163e3da0662109f768b6b67495a644774575c
Valery Yakovlev <vd.yakovlev@mpoffice.ru>
2018-12-09 00:04:09 +0300

    cors added

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-var cors = require('cors')
+
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
 
 const app = express();
-
-app.use(cors())
+app.use(allowCrossDomain);
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/car-performance-ui'));
