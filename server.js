d5dd0c810d9e617672e1b7936ed15e4b57004ad9
Valery Yakovlev <vd.yakovlev@mpoffice.ru>
2018-12-08 23:51:53 +0300

    cors added

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var cors = require('cors')
 
 const app = express();
+
+app.use(cors())
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/car-performance-ui'));
